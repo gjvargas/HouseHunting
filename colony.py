d9c5186952adf13583be0ffb97c4e233587885d6
Giancarlo Sturla <giancarlo.sturla7@gmail.com>
2015-11-11 23:14:42 -0500

    Added list comp to nest init

--- colony.py
+++ colony.py
@@ -1,15 +1,11 @@
 from ant import *
+import random
 
 class Colony:
     def __init__(self, size, numNests):
-        self.ants = []
+        self.ants = [Ant(i) for i in range(size)]
         self.size = size
-        self.nests = []
-        for i in range(numNests):
-
-        self.nests = nests
-        for i in range(size):
-            self.ants.append(Ant(i))
+        self.nests = [Nest(random.randInt(0,1) for i in range(numNests)]
 
     def step(self):
         for ant in self.ants:
