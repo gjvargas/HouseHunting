7fec6a0161414ea8faee528d81380a2a3cd6935a
Giancarlo Sturla <giancarlo.sturla7@gmail.com>
2015-12-06 19:29:26 -0500

    nonbinary

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from colony import *
+from nonbinary import *
+import pickle
 
 def main():
-    colony = Colony(1000, 100)
-    colony.go()
+    for x in range(30):
+    	print 'started', x
+        trials = [(i * 50, 20) for i in range(1, 21)]
+        output = []
+        for numAnts, numNests in trials:
+            # print("starting trial", numAnts, numNests)
+            colony = Colony(numAnts, numNests)
+            numSteps, chosen_quality, best_quality = colony.go()
+            dataPoint = {"ants": numAnts, "nests": numNests, "steps": numSteps, "chosen": chosen_quality, "best": best_quality}
+            output.append(dataPoint)
+            # print("just finished trial", numAnts, numNests)
+        pickle.dump(output, open("results/ants" + str(x) + ".p", "w"))
+        print 'finished', x
 
 if __name__ == "__main__":
     main()
