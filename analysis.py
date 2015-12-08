30bb248840f7552e5c48524118635152d84b19b9
Giancarlo Sturla <giancarlo.sturla7@gmail.com>
2015-12-08 17:27:55 -0500

    Automated step and quality plots

--- analysis.py
+++ analysis.py
@@ -2,14 +2,14 @@ import cPickle as pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-def aggregateData(numTrials, numTests):
+def aggregateData(numTrials, numTests, folder):
     ants = np.zeros((numTrials,numTests))
     nests = np.zeros((numTrials,numTests))
     steps = np.zeros((numTrials,numTests))
     bestQualities = np.zeros((numTrials,numTests))
     chosenNests = np.zeros((numTrials,numTests),dtype=object)
     for i in range(numTrials):
-        data = pickle.load(open("results/ants"+str(i)+".p","r"))
+        data = pickle.load(open(folder+"/ants"+str(i)+".p","r"))
         for j in range(len(data)):
             ants[i][j] = data[j]["ants"]
             nests[i][j] = data[j]["nests"]
@@ -18,18 +18,29 @@ def aggregateData(numTrials, numTests):
             chosenNests[i][j] = data[j]["chosenNest"]
     return ants,nests,steps,bestQualities,chosenNests
 
-def plotSteps(ants,steps):
+def plotSteps(ants,controlSteps,nonbinarySteps):
     x = ants[1,:]
-    y = np.median(steps,axis=0)
-    plt.plot(x,y)
+    controlMedian = np.median(controlSteps,axis=0)
+    nonbinaryMedian = np.median(nonbinarySteps,axis=0)
+
+    control, = plt.plot(x,controlMedian,label="control")
+    nonbinary, = plt.plot(x,nonbinaryMedian,label="nonbinary")
+    plt.legend([control,nonbinary],["Control","Nonbinary"])
     plt.show()
 
-def plotChosenQualities(ants,chosenNests,bestQualities):
+def plotChosenQualities(ants,controlChosenNests,controlBestQualities,nonbinaryChosenNests,nonbinaryBestQualities):
     x = ants[1,:]
     def extractQuality(nest):
         return nest.quality
     vfunc = np.vectorize(extractQuality)
-    chosenQualities = vfunc(chosenNests)
-    y = np.median((chosenQualities-bestQualities)/bestQualities,axis=0)
-    plt.plot(x,y)
+
+    controlChosenQualities = vfunc(controlChosenNests)
+    nonbinaryChosenQualities = vfunc(nonbinaryChosenNests)
+
+    controlMedian = np.median((controlChosenQualities-controlBestQualities)/controlBestQualities,axis=0)
+    nonbinaryMedian = np.median((nonbinaryChosenQualities-nonbinaryBestQualities)/nonbinaryBestQualities,axis=0)
+    
+    control, = plt.plot(x,controlMedian,label="control")
+    nonbinary, = plt.plot(x,nonbinaryMedian,label="nonbinary")
+    legend = plt.legend([control,nonbinary],['control','nonbinary'])
     plt.show()
